05baf98095ab1c5436a868cade8aa40f5f3ce0a3
William Wang <william.wang@shopify.com>
2021-02-27 16:06:43 -0500

    Add Admin, Teacher, and Student schema changes
    Co-authored-by: alexatshopify <alex.djordjevic@shopify.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_202141) do
+ActiveRecord::Schema.define(version: 2021_02_27_203714) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "fname"
