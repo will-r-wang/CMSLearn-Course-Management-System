19fdf1e764735884104fc6b10b6c90d452f8c0c1
Erin Son <erin.son@shopify.com>
2021-03-05 09:21:00 -0500

    [Functionality] Add Quiz model
    This model represents quizzes and its children in our CMS.

---
+++ db/migrate/20210305141835_create_quizzes.rb
@@ -0,0 +1,11 @@
+class CreateQuizzes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :quizzes do |t|
+      t.datetime :time_opened
+      t.datetime :time_closed
+      t.time :alloted_time
+
+      t.timestamps
+    end
+  end
+end
