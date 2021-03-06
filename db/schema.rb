858673fa3f1bc4c816c0006c6226b77782da7329
William Wang <william.wang@shopify.com>
2021-03-06 13:41:51 -0500

    [Functionality] Add teacher_id column to teachers
    This adds the teacher_id attribue to the teacher model which is a type of
    user username in our CMS. This will be autogenerated upon teacher
    creation and will be used by all teachers to login.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_223027) do
+ActiveRecord::Schema.define(version: 2021_03_06_184057) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_223027) do
   create_table "teachers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "teacher_id"
   end
 
   create_table "users", force: :cascade do |t|
