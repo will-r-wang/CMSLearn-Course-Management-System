0b1662941220d05c09dd397614c29050818335f0
Erin Son <erin.son@shopify.com>
2021-03-07 14:16:38 -0500

    [Functionality] Add Activity model
    This model represents all course activities tracked in our CMS.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_191148) do
+ActiveRecord::Schema.define(version: 2021_03_07_191542) do
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.integer "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_activities_on_course_id"
+  end
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -104,4 +112,5 @@ ActiveRecord::Schema.define(version: 2021_03_07_191148) do
     t.string "email"
   end
 
+  add_foreign_key "activities", "courses"
 end
