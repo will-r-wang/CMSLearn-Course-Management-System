729d991f2f02dfa116b7480fc679ad0bd5af33af
William Wang <william.wang@shopify.com>
2021-03-30 01:15:13 -0400

    [Testing] Fix subscription and sessions_controller tests
    Now that we've implemented bcrypt for user authentication, the password
    validation exists on the user such that each user must have a password.
    This sets a test password for the Student in the subscription_test.
    
    Also deleted several sessions_controller tests since the associated
    paths were 🔥d.

--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SubscriptionTest < ActiveSupport::TestCase
   test "initialize creates a subscription with student_id and announcement_manager_id" do
-    student = Student.create
+    student = Student.create(password: 'test')
     announcement_manager = AnnouncementManager.create
     assert_difference 'Subscription.count', 1 do
       Subscription.create(student_id: student.id, announcement_manager_id: announcement_manager.id)
