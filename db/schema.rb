21e5578f93a23f2d228ae9cc3d7c98b7b5a3ea0c
William Wang <william.wang@shopify.com>
2021-04-04 01:43:20 -0400

    [Refactoring] Rename registration to course_registration
    This renames registration to course_registration now that we plan to
    implement a type of user_registration. Being more specific and
    intentional in our naming will prevent misclarity in the future.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_064150) do
+ActiveRecord::Schema.define(version: 2021_04_04_053712) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 2021_03_29_064150) do
   create_table "background_surveys", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "course_registrations", force: :cascade do |t|
+    t.string "status"
+    t.integer "student_id", null: false
+    t.integer "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_course_registrations_on_course_id"
+    t.index ["student_id"], name: "index_course_registrations_on_student_id"
   end
 
   create_table "courses", force: :cascade do |t|
@@ -91,16 +101,6 @@ ActiveRecord::Schema.define(version: 2021_03_29_064150) do
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "registrations", force: :cascade do |t|
-    t.string "status"
-    t.integer "student_id", null: false
-    t.integer "course_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["course_id"], name: "index_registrations_on_course_id"
-    t.index ["student_id"], name: "index_registrations_on_student_id"
   end
 
   create_table "resources", force: :cascade do |t|
@@ -162,6 +162,6 @@ ActiveRecord::Schema.define(version: 2021_03_29_064150) do
   end
 
   add_foreign_key "activities", "courses"
-  add_foreign_key "registrations", "courses"
-  add_foreign_key "registrations", "students"
+  add_foreign_key "course_registrations", "courses"
+  add_foreign_key "course_registrations", "students"
 end
