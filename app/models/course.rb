831adb918022ffe589e0d50bddeafc548dd12f33
William Wang <william.wang@shopify.com>
2021-04-05 02:28:53 -0400

    [Functionality] Fix model associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,10 @@ class Course < ApplicationRecord
   belongs_to :teacher
   has_many :activity
   has_many :student_courses
-  has_many :students, through: student_courses
+  has_many :students, through: :student_courses
   has_many :announcement
+
+  def enrolled_students
+    student_courses.count
+  end
 end
