9d43c1da2bb47631fbfa67b1a1fa1729587f27fe
William Wang <william.wang@shopify.com>
2021-04-08 18:13:29 -0400

    [Functionality] Fixup student<>course and add course view
    This fixes up the course_registration association on the Student model
    to instead be user_id since we're using single table inheritance. Also
    adds the student course view whereby students can see a list of all
    their currently registered courses and click to view course.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,2 @@
 class Student < User
-  has_many :course_registrations, dependent: :delete_all
-  has_many :courses, through: :course_registrations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   belongs_to :user_registration, optional: true
   has_many :user_registrations, dependent: :delete_all
+  has_many :course_registrations, dependent: :delete_all
+  has_many :courses, through: :course_registrations
+
   has_secure_password
 
   def full_name
