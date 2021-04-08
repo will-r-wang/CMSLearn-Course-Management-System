8f297d06e3963c966a5bc42100422a1a7cc2b38d
William Wang <william.wang@shopify.com>
2021-04-08 18:33:33 -0400

    [Refactoring] Move helpers out of application_controller
    Having helper_method in application_controller is a bit of a code smell
    following Rails conventions that we want to avoid in the long run. This
    moves all of the helper methods into the ApplicationHelper and includes
    it in the controller instead.
    
    Also replaces some of the unnecessary helpers with the direct reference
    and deletes the empty helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def current_semester
+    Semester.where("start_date <= ?", Time.now).where("end_date >= ?", Time.now).first
+  end
+
+  def pending_user_registration?
+    logged_in? && !UserRegistration.where(user_id: current_user.id, status: "pending").empty?
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def admin?
+    logged_in? && !Admin.find_by(username: current_user.username).nil?
+  end
+
+  def student?
+    logged_in? && !Student.find_by(username: current_user.username).nil?
+  end
+
+  def teacher?
+    logged_in? && !Teacher.find_by(username: current_user.username).nil?
+  end
 end
