4f82da54d831611ee2e608c555bde1ca7d5e1086
William Wang <william.wang@shopify.com>
2021-04-11 01:56:57 -0400

    [Design Pattern] Display SDC and timeline charts
    This makes use of our previously implemented SDC pattern to create a
    graphic visualization of student grades across our Course Management
    System. Also adds a timeline chart with the help of the GoogleCharts API
    to display the timelines of all semesters.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
     if current_user.type == "Admin"
       @user_registrations = UserRegistration.where(status: "pending") 
       @course_registrations = CourseRegistration.includes(:user, :course).where(status: "pending")
+      @chart_data = ChartDataGet.new(
+        CourseRegistration.where('grade is NOT NULL').pluck(:user_id, :grade)
+      ).formatted_data
+      @semester_timelines = Semester.all.map { |semester| [semester.name, semester.start_date.strftime("%Y-%B-%d"), semester.end_date.strftime("%Y-%B-%d")] }
     end
     @courses_teaching = Course.includes(:semester).where(teacher_id: current_user.id) if current_user.type == "Teacher"
     @user_registration = UserRegistration.find_by(user_id: current_user.id, status: "pending")
