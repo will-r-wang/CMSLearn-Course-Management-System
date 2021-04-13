0f4e50d44018f305f90e27fb4e21df9a85322ab3
William Wang <william.wang@shopify.com>
2021-04-12 23:01:09 -0400

    [Hotfix] Fix a broken unit test with incorrect kwarg params

--- test/models/user_registration_test.rb
+++ test/models/user_registration_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
 class UserRegistrationTest < ActiveSupport::TestCase
-
   test "create a new user registration" do
     user = User.create!(username: "test", password: "test")
-    user_registration = UserRegistration.new(status: "pending", user_type: "student", user_id: user)
+    user_registration = UserRegistration.new(status: "pending", user_type: "student", user: user)
     assert user_registration.save
   end
-
 end
